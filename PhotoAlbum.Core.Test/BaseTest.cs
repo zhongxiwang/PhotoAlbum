d6d57257e00c5466c823a76f8461af95bc508674
junmoxiao <zhongxiwnag@live.com>
2020-06-16 07:35:23 +0800

    添加测试项目

---
+++ PhotoAlbum.Core.Test/BaseTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.Core.Test
+{
+    class BaseTest
+    {
+    }
+}
---
+++ PhotoAlbum.Core/Elastic/IElasticSearch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.Core.Elastic
+{
+    public interface IElasticSearch
+    {
+    }
+}
