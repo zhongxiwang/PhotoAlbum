da044c74bafb5204abc6e6a0a79898fa7b836372
zxw <zhongxiwang@live.com>
2020-06-17 19:38:12 +0800

    1

---
+++ PhotoAlbum.Core/Model/PhotoAlbumModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.Core.Model
+{
+    public class PhotoAlbumModel
+    {
+        public PhotoAlbumModel()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        /// <summary>
+        /// 相册名称
+        /// </summary>
+        public string Name { get; set; }
+
+        public string Cover { get; set; }
+        public string Id { get;  set; }
+        public string Description { get; set; } 
+        public DateTime CreateTime { get; set; }
+        public int Total { get; set; }
+        
+    }
+}
---
+++ PhotoAlbum.Core/Services/Request.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.Core.Services
+{
+    class Request
+    {
+    }
+}
